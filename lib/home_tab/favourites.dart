c10318edebb5373bc7076aef8733f80a71f30c9e
New <gedofadol2017@gmail.com>
2021-04-04 23:38:20 +0300

    Initial project setup

--- lib/home_tab/favourites.dart
+++ lib/home_tab/favourites.dart
@@ -1,3 +1,5 @@
+
+import 'dart:math';
 import 'package:flutter/material.dart';
 class Favourites extends StatefulWidget {
   @override
@@ -5,10 +7,141 @@ class Favourites extends StatefulWidget {
 }
 
 class _FavouritesState extends State<Favourites> {
+
+  List<Color> colorsList = [
+    Colors.deepPurpleAccent,
+    Colors.green,
+    Colors.amber,
+    Colors.red,
+    Colors.pink,
+    Colors.lightBlue,
+    Colors.deepOrangeAccent,
+    Colors.indigo
+  ];
+
+  Random random = Random();
+
+  Color _getRandomColor(){
+    return colorsList[ random.nextInt(colorsList.length)];
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.orange,
+    return ListView.builder(
+        itemBuilder: (context, position){
+          return Card(
+            child: Container(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  _authorRow(),
+                  SizedBox(height: 16,),
+                  _newsItemRow()
+                ],
+              ),
+            ),
+          );
+        }, itemCount: 20,
+    );
+  }
+  Widget _authorRow(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: ExactAssetImage
+                      ("assets/images/gg2.jpg"),
+                  fit: BoxFit.cover,
+                ),
+                    shape: BoxShape.circle,
+              ),
+              width: 40,
+              height: 40,
+              margin: EdgeInsets.only(right: 16),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                    "Michael Adams",
+                    style: TextStyle(
+                    color: Colors.black,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600
+
+                  ),
+                ),
+                SizedBox(height: 8,),
+                Row(
+                  children: [
+                    Text(
+                      "15 mit" ,
+                      style: TextStyle(
+                          color: Colors.blueGrey
+                      ),
+                    ),
+                    Text("Life Style" ,
+                        style: TextStyle(
+                        color: _getRandomColor() ,
+                        fontSize: 14 ,
+                        fontWeight: FontWeight.w600
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+        IconButton(icon: Icon(Icons.bookmark_border) ,
+            onPressed: () {},
+            color: Colors.blueGrey,
+          )
+      ],
+    );
+  }
+  Widget _newsItemRow(){
+    return Row(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: ExactAssetImage
+                    ("assets/images/gg.jpg"),
+                  fit: BoxFit.cover,
+              ),
+          ),
+          width: 140,
+          height: 120,
+          margin: EdgeInsets.only(right: 16),
+        ),
+        Expanded(
+          child: Column(
+            children: [
+              Text(
+                "Tech tent: Old phones and safe social",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 19,
+                  fontWeight: FontWeight.w600
+                ),
+              ),
+              SizedBox(height: 8,),
+              Text(
+                "we also talk about the  of  as the robots  we also talk about the  of  as the robots ",
+                style: TextStyle(
+                  color: Colors.blueGrey,
+                  fontSize: 16,
+                  height: 1.5
+                ),
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 }
--- lib/home_tab/popular.dart
+++ lib/home_tab/popular.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 class Popular extends StatefulWidget {
   @override
@@ -7,8 +8,62 @@ class Popular extends StatefulWidget {
 class _PopularState extends State<Popular> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.brown,
+    return ListView.builder(
+        itemBuilder:(context , position) {
+        return Card(
+          child: _drawSingleRow(),
+        );
+        },
+      itemCount: 20,
+    );
+  }
+  Widget _drawSingleRow(){
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: Row(
+        children: <Widget>[
+          SizedBox(
+            child:
+            Image(image: ExactAssetImage("assets/images/gg.jpg"),
+              fit: BoxFit.cover,
+            ),
+            width: 100,
+            height: 100,
+          ),
+          SizedBox(
+            width: 16,
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Text("The world Glodal warming annual sumit",
+                  maxLines: 2,
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                SizedBox(
+                  height: 18,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment
+                      .spaceBetween,
+                  children: [
+                    Text("Michel Adams"),
+                    Row(
+                      children: [
+                        Icon(Icons.timer),
+                        Text("15 min"),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
